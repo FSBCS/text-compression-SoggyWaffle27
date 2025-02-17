94991494462fe7e0936b6727a83e944c93b54c79
Ben Long <155552749+SoggyWaffle27@users.noreply.github.com>
2025-02-17 16:15:47 +0000

    Finished Version

--- huffman.py
+++ huffman.py
@@ -15,41 +15,46 @@ class HuffmanEncoding:
         self.encoded_text = encoded_text
         self.root = root
         if self.encoded_text != None:
-            self.encoded_text = self.encoding()
+            output = ""
+            dict = self._build_dictionary(self.root)
+            dict = {v: k for k, v in dict.items()}
+            substring = ""
+            for char in str(self.encoded_text):
+                substring += char
+                if substring in dict:
+                    output += dict[substring]
+                    substring = ""
+            self.decoded_text = output
         else:
-            self.encoded_text = self.encoding()
+            out = ""
+            items = self.frequency(self.src)
+            PQ = self.PriorityQueue()
+            for i in items:  #yeah so I forgot we had a PQ and I had already made my own so I just repurposed it
+                PQ.insert(self.Node(i[0], i[1]))
+            while len(PQ.queue) != 2:
+                min1 = PQ.findMin()[0]
+                min2 = PQ.findMin()[0]
+                PQ.insert(self.Node(freq = (min1.freq + min2.freq), left=min2, right=min1))
+            self.root = PQ.queue[1]
+            dict = self._build_dictionary()
+            for i in self.src:
+                out += dict[i]
+            self.encoded_text = out
 
     class Node:
         def __init__(self, freq, char=None, left=None, right=None):
             self.char = char
             self.freq = freq
-            self.left = left
-            self.right = right
+            self.left = left 
+            self.right = right 
         
         def is_leaf(self):
             return self.char is not None
-    def encoding(self, out = ""):
-        items = self.frequency(self.src)
-        PQ = self.PriorityQueue()
-        for i in items:  #yeah so I forgot we had a PQ and I had already made my own so I just repurposed it
-            PQ.insert(self.Node(i[0], i[1]))
-        while len(PQ.queue) != 2:
-            min1 = PQ.findMin()[0]
-            min2 = PQ.findMin()[0]
-            PQ.insert(self.Node(freq = (min1.freq + min2.freq), left=min2, right=min1))
-        self.root = PQ.queue[1]
-        dict = self._build_dictionary()
-        for i in self.src:
-            out += dict[i]
-        return out
             
     def frequency(self, str):
         dict = {}
-        for i in str:
-            if i in dict:
-                dict[i] += 1
-            else:
-                dict[i] = 1
+        for char in str:
+            dict[char] = dict.get(char,0) + 1
         dict = sorted(dict.items(), key=lambda item: item[1], reverse = True)
         dict = [(v, k) for k, v in dict[::-1]]
         return dict
@@ -121,5 +126,13 @@ class HuffmanEncoding:
             del(self.queue[min[1]])
             return min
             
-encoding = HuffmanEncoding("AAAAAABBBBBCCCCDDEEF")
-print(len(encoding.encoded_text))
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+file_path = os.path.join(os.path.dirname(__file__), 'test_data', 'totc.txt')
+with open(file_path, 'r') as file:
+    contents = file.read()
+
+encoding = HuffmanEncoding(src = contents)
+print("\nLength Test:\n", " - Encoded text length:", len(encoding.encoded_text), "\n  - ASCII text length:", len(contents) * 8 , "\n") #Huffman vs ASCII
+decode = HuffmanEncoding(encoded_text = encoding.encoded_text, root = encoding.root)
+print(decode.decoded_text)
